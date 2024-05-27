4d91b2ee302209c521e10ccd44e1b88c077f312e
mehreenwaqar <mehreenwaqar96@gmail.com>
2024-05-27 23:51:55 +0500

    first commit

---
+++ TextEditor/Source/ZSSBarButtonItem.h
@@ -0,0 +1,12 @@
+//
+//  ZSSBarButtonItem.h
+//  ZSSRichTextEditor
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZSSBarButtonItem : UIBarButtonItem
+
+@property (nonatomic, strong) NSString *label;
+
+@end
